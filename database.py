eabd11fb0cf3c892b48811136cfca8f42a026e1c
gaberay2022 <gaberay2002@yahoo.com>
2024-02-05 12:48:43 -0600

    Updating database class, main, and creating config/datafiles

--- database.py
+++ database.py
@@ -6,22 +6,94 @@ class DB:
         self.recordSize = 0
         self.numRecords = 0
         self.dataFileptr = None
+        self.Id_size = 10
+        self.fname_size = 25
+        self.lname_size = 25
+        self.age_size = 4
+        self.ticketNum_size = 25
+        self.fare_size = 8
+        self.date_size = 15
+
+    def createDatabase(self, filename):
+        csv_filename = filename + ".csv"
+        data_filename = filename + ".data"
+        config_filename= filename + ".config"
+
+        with open(csv_filename, "r") as csv_file:
+            data_list = list(csv.DictReader(csv_file, fieldnames = ('passengerID', 'fname', 'lname', 'age', 'ticketNum', 'fare', 'date')))
+
+        def writeDB(filestream, dict):
+            filestream.write("{:{width}.{width}}".format(dict["passengerID"],width=self.Id_size))
+            filestream.write("{:{width}.{width}}".format(dict["fname"],width=self.fname_size))
+            filestream.write("{:{width}.{width}}".format(dict["lname"],width=self.lname_size))
+            filestream.write("{:{width}.{width}}".format(dict["age"],width=self.age_size))
+            filestream.write("{:{width}.{width}}".format(dict["ticketNum"],width=self.ticketNum_size))
+            filestream.write("{:{width}.{width}}".format(dict["fare"],width=self.fare_size))
+            filestream.write("{:{width}.{width}}".format(dict["date"],width=self.date_size))
+            filestream.write("\n")
+
+            filestream.write("{:{width}.{width}}".format('__empty__',width=self.Id_size))
+            filestream.write("{:{width}.{width}}".format('',width=self.fname_size))
+            filestream.write("{:{width}.{width}}".format('',width=self.lname_size))
+            filestream.write("{:{width}.{width}}".format('',width=self.age_size))
+            filestream.write("{:{width}.{width}}".format('',width=self.ticketNum_size))
+            filestream.write("{:{width}.{width}}".format('',width=self.fare_size))
+            filestream.write("{:{width}.{width}}".format('',width=self.date_size))
+            filestream.write("\n")
+
+        with open(data_filename, "w") as outfile:
+            for dict in data_list:
+                writeDB(outfile, dict)
+
+        recordSize = self.Id_size + self.fname_size + self.lname_size + self.age_size + self.ticketNum_size + self.fare_size + self.date_size + 1
     
+        with open(config_filename, "w") as config_file:
+            config_file.write(f"Number of records: {len(data_list)*2}\n")
+            config_file.write(f"Record size: {recordSize}\n")
+
     def open(self, fileName):
-        csv_file = f"{fileName}.csv"
-        data_file = f"{fileName}.data"
+        config_filename = f"{fileName}.config"
+        data_filename = f"{fileName}.data"
+
+        #open Config File
         try:
-            with open(csv_file, "r") as csv_file:
-                self.numRecords, self.recordSize = map(int, csv_file.readline().split())
-            self.dataFileptr = open(data_file, "r+")
-            return True
-        except:
-            print(f"Database {fileName} not found.")
+            with open(config_filename, "r") as config_file:
+                for line in config_file:
+                    if "Number of records:" in line:
+                        self.numRecords = int(line.split(":")[1].strip())
+                    elif "Record size:" in line:
+                        self.recordSize = int(line.split(":")[1].strip())
+
+        except FileNotFoundError:
+            print(f"Config file {config_filename} not found.")
             return False
+        
+        except ValueError:
+            print("Error reading the config file.")
+            return False
+
+        # Open the Data file
+        try:
+            if self.isOpen():
+                print("Please close pervious Database beore opening a new one.")
+
+                return False
+                
+            else:
+                self.dataFileptr = open(data_filename, "r+")
+                print(f"Database {fileName} is open")
+
+
+        except FileNotFoundError:
+            print(f"Data file {data_filename} not found.")
+            return False
+
+        return True
     
     def close(self):
         if self.dataFileptr:
             self.dataFileptr.close()
+            print("Database Closed")
         self.recordSize = 0
         self.numRecords = 0
         self.dataFileptr = None
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from database import DB
 
 def main():
-    dbObject = DB()
+    db = DB()
     while True:
         display_menu()
         try:
@@ -9,7 +9,7 @@ def main():
             if userInput == 9:
                 print("Quitting the program. Goodbye!")
                 break  # Exit the loop and the program
-            process_user_input(userInput, dbObject)
+            process_user_input(userInput, db)
         except ValueError:
             print("Invalid input. Please enter a number between 1 and 9.")
 
@@ -26,31 +26,42 @@ def display_menu():
     print("8 - Delete a Record")
     print("9 - Quit the Program")
 
-def process_user_input(userInput, dbObject):
+def process_user_input(userInput, db):
     if userInput == 1:
         print("You have chosen to create a new database.")
-        # Call a function to create a new database
+        filename = input("please input the name of the csv file: ")
+        db.createDatabase(filename)
+        
     elif userInput == 2:
         print("You have chosen to open an existing database.")
-        # Call a function to open an existing database
+        filename = input("please input the name of the database you would like to open: ")
+        db.open(filename)
+        
     elif userInput == 3:
         print("You have chosen to close the database.")
-        # Call a function to close the database
+        db.close()
+        
     elif userInput == 4:
         print("You have chosen to display a record.")
-        # Call a function to display a record
+        recordNum = int(input("please input the reocrd number you would like to view: "))
+        db.readRecord(recordNum)
+        
     elif userInput == 5:
         print("You have chosen to update a record in the database.")
         # Call a function to update a record
+        
     elif userInput == 6:
         print("You have chosen to create a report for the database.")
         # Call a function to create a report
+        
     elif userInput == 7:
         print("You have chosen to add a record to the database.")
         # Call a function to add a record
+        
     elif userInput == 8:
         print("You have chosen to delete a record from the database.")
         # Call a function to delete a record
+        
     else:
         print("Invalid option. Please enter a number between 1 and 9.")
 
